da293030ebc3b2cd63a84c1eaf2b366e7832aba4
tim3hy <tim.christophe.graf@gmail.com>
2020-07-03 08:28:56 +0200

    Initial commit

---
+++ app/src/main/java/ch/zli/m335/textpinion1/MainActivity.java
@@ -0,0 +1,38 @@
+package ch.zli.m335.textpinion1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button buttonTxtAnal;
+    Button buttonOptionen;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        buttonOptionen = (Button) findViewById(R.id.buttonOptionen);
+        buttonTxtAnal = (Button) findViewById(R.id.buttonTxtAnal);
+
+        buttonTxtAnal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTxtAnal = new Intent(MainActivity.this, TextEingabe.class);
+                startActivity(intentTxtAnal);
+            }
+        });
+        buttonOptionen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentOptionen = new Intent(MainActivity.this, Optionen.class);
+                startActivity(intentOptionen);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/ch/zli/m335/textpinion1/TextEingabe.java
@@ -0,0 +1,23 @@
+package ch.zli.m335.textpinion1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+public class TextEingabe extends AppCompatActivity {
+
+    EditText textEingabe;
+    Button txtPinion;
+    ImageButton imageButton;
+    String text;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_eingabe);
+
+    }
+}
