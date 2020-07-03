7f7edb0f967012cb3ff71a45844620b9c09de1e1
tim3hy <tim.christophe.graf@gmail.com>
2020-07-03 10:08:03 +0200

    API trying

--- app/src/main/java/ch/zli/m335/textpinion1/TextEingabe.java
+++ app/src/main/java/ch/zli/m335/textpinion1/TextEingabe.java
@@ -9,12 +9,19 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 
+import com.android.volley.RequestQueue;
+import com.aylien.textapi.TextAPIClient;
+import com.aylien.textapi.TextAPIException;
+import com.aylien.textapi.parameters.SentimentParams;
+import com.aylien.textapi.responses.Sentiment;
+
 public class TextEingabe extends AppCompatActivity {
 
     EditText textEingabe;
     Button txtPinion;
     ImageButton imageButton;
-    String text;
+    String textView;
+    private RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,9 +45,24 @@ public class TextEingabe extends AppCompatActivity {
         txtPinion.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                //txtPinion();
                 Intent intentTxtPinion = new Intent(TextEingabe.this, TextPinion.class);
                 startActivity(intentTxtPinion);
             }
         });
+    }
+
+    private void txtPinion(){
+
+        TextAPIClient client = new TextAPIClient("5980b674", "be242863b1cc89da35f9034f597b762e");
+        SentimentParams.Builder builder = SentimentParams.newBuilder();
+        builder.setText("John is a very good football player");
+        Sentiment sentiment = null;
+        try {
+            sentiment = client.sentiment(builder.build());
+        } catch (TextAPIException e) {
+            e.printStackTrace();
+        }
+        System.out.println(sentiment);
     }
 }
