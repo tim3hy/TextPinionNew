0ab1246f6389b5f7c837f418bce3cf1fe7b9deb1
tim3hy <tim.christophe.graf@gmail.com>
2020-07-03 09:01:37 +0200

    Main Activity finished

--- app/src/main/java/ch/zli/m335/textpinion1/TextPinion.java
+++ app/src/main/java/ch/zli/m335/textpinion1/TextPinion.java
@@ -2,13 +2,28 @@ package ch.zli.m335.textpinion1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class TextPinion extends AppCompatActivity {
+
+    ImageButton imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_pinion);
+
+        imageButton = (ImageButton) findViewById(R.id.imageButton);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentTxtEingabe = new Intent(TextPinion.this, TextEingabe.class);
+                startActivity(intentTxtEingabe);
+            }
+        });
     }
 }
